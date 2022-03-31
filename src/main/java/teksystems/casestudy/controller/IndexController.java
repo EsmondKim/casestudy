56a6379c6c2c534476b7bce9e0534e36da6b85f5
EsmondKim <ekelectronicmail@gmail.com>
2022-03-31 17:09:02 -0500

    After class, Thursday 03312022

--- src/main/java/teksystems/casestudy/controller/IndexController.java
+++ src/main/java/teksystems/casestudy/controller/IndexController.java
@@ -1,16 +1,38 @@
 package teksystems.casestudy.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import teksystems.casestudy.database.entity.User;
+import teksystems.casestudy.database.dao.UserDAO;
 
+
+import java.util.List;
+
+@Slf4j
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserDAO userDAO;
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public ModelAndView index() throws Exception {
         ModelAndView response = new ModelAndView();
+
+        List<User> firstName = userDAO.findByFirstName("Eric");
+        List<User> firstLetter = userDAO.findByFirstNameIgnoreCaseContaining("E");
+
+        for( User name : firstName ) {
+            log.info(name.toString());
+        }
+
+        for( User name : firstLetter ) {
+            log.info(name.toString());
+        }
 
         response.setViewName("index");
 
---
+++ src/main/java/teksystems/casestudy/database/dao/UserDAO.java
@@ -0,0 +1,23 @@
+package teksystems.casestudy.database.dao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import teksystems.casestudy.database.entity.User;
+
+import java.util.List;
+
+@Repository
+public interface UserDAO extends JpaRepository<User, Long> {
+    public User findByEmail(@Param("email") String email);
+
+    public List<User> findByFirstName(@Param("firstName") String firstName);
+
+    public List<User> findByFirstNameIgnoreCaseContaining(@Param("firstName") String firstName);
+
+//    public List<User> findByFirsNameAndLast_name(@Param("firstName") String firstName, @Param("lastName") String lastName);
+
+    @Query(value = "select u from User u where u.password = :password")
+    public List<User> getByPassword(@Param("password") String password);
+
+}
